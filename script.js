561ee7942f7e3e65c1058da95d4f7df05cacd812
mark <ragzouken@gmail.com>
2020-02-25 12:56:49 +0000

    cleaner coords

--- script.js
+++ script.js
@@ -11,26 +11,6 @@ async function loaded() {
     const json = document.getElementById('data').innerText;
     const data = JSON.parse(json);
 
-    /*
-    for (let radius = 1; radius < 3; ++radius) {
-        let cube = cubeAdd([0, 0, 0], cubeScale(cube_directions[4], radius));
-        for (let i = 0; i < 6; ++i) {
-            for (let j = 0; j < radius; ++j) {
-                cube = cubeNeighbor(cube, i);
-
-                if (Math.random() > .8) continue;
-
-                const [q, r, s] = cube;
-                data['cards'].push({
-                    'coords': [q, r],
-                    'text': '"digital technology is a product of cheap energy"',
-                    'type': randomInt(0, 2),
-                });
-            }
-        }
-    }
-    */
-
     const testCard = document.createElement('div');
     testCard.classList.add('card');
     main.appendChild(testCard);
@@ -41,22 +21,14 @@ async function loaded() {
 
     main.removeChild(testCard);
 
-    let [tx, ty] = [0, 0];
     function setTranslate(x, y) {
-        tx = x;
-        ty = y;
-        main.style.transform = 'translate(' + tx + 'px, ' + ty + 'px)';
+        main.style.transform = `translate(${x}px, ${y}px)`;
     }
 
-    function centerCard(div, card) {
-        const divRect = div.getBoundingClientRect();
-
-        const [x, y] = [(divRect.left + divRect.right) / 2, 
-                        (divRect.top + divRect.bottom) / 2];
+    function centerCoords(coords) {
         const [cx, cy] = [document.documentElement.clientWidth / 2, document.documentElement.clientHeight / 2];
-        const [dx, dy] = [cx - x, cy - y];
-
-        setTranslate(tx + dx, ty + dy);
+        const [nx, ny] = hexToPixel(coords, [cardWidth, cardHeight], spacing);
+        setTranslate(cx - nx , cy - ny);
     }
 
     function addCard(card) {
@@ -79,7 +51,7 @@ async function loaded() {
         main.appendChild(div);
 
         div.addEventListener('click', () => {
-            centerCard(div, card);
+            centerCoords(card['coords']);
         });
 
         cards.set(coords, {card, div});
@@ -94,12 +66,8 @@ async function loaded() {
 
     function repositionCard(card) {
         const [x, y] = hexToPixel(card['coords'], [cardWidth, cardHeight], spacing);
-
         div = cards.get(coordsToKey(card['coords'])).div;
-
-        div.style.position = "absolute";
-        div.style.left = x + "px";
-        div.style.top = y + "px";
+        div.style.transform = `translate(calc(${x}px - 50%), calc(${y}px - 50%))`;
     }
 
     function swapCells(a, b) {
@@ -111,8 +79,6 @@ async function loaded() {
         
         const ac = cards.get(akey);
         const bc = cards.get(bkey);
-
-        //console.log(`${ac.card['text']} <-> ${bc.card['text']}`);
 
         if (ac)
             deleteCard(ac.card);
@@ -152,6 +118,15 @@ async function loaded() {
         addCard(card);
         repositionCard(card);
     }
+
+    main.classList.add('skiptransition');
+    centerCoords([0, 0]);
+    await sleep(10);
+    main.classList.remove('skiptransition');
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function cubeAdd(a, b) {
